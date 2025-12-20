54159b5c276adb23b12c8a578ba797cf59de115f
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-20 17:17:47 +0700

    working_bento_grid

--- src/app/competitions/sports/page.tsx
+++ src/app/competitions/sports/page.tsx
@@ -4,6 +4,7 @@ import CompetitionPicker from "@/components/competition/CompetitionPicker";
 import StripeBackground from "@/components/StripeBackground";
 import RectorInlineTitle from "@/components/competition/RectorInlineTitle";
 import BottomAsset from "@/components/competition/BottomAsset";
+
 export default async function page() {
   return (
     <>
@@ -14,50 +15,65 @@ export default async function page() {
         <div className="relative z-2 my-[10rem] md:my-[10%] flex flex-col gap-4 w-[90%] justify-center items-center border-2 sm:border-4 border-[#AAF3D5] p-12 rounded-lg shadow-lg backdrop-blur-2xl bg-gradient-to-b from-[#390D62]/40 to-[#6226A4]/40">
           <RectorInlineTitle />
           <div className="w-full border-2 sm:border-4 border-[#AAF3D5] rounded-2xl py-2 bg-gradient-to-r from-[#6427A8] to-[#EB79F0] flex justify-center items-center">
-            <h1 className="text-2xl">COMPETITIONS</h1>
+            <h1 className="text-2xl text-white font-bold">COMPETITIONS</h1>
           </div>
-          <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
+          
+          <div className="w-full grid grid-cols-1 md:grid-cols-6 md:grid-rows-7 gap-4">
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-              className="md:col-span-2 md:row-span-1"
-            ></CompetitionPicker>
+              link="/competitions/sports/basketball"
+              title="Basketball"
+              alt="rectorcup"
+              className="w-full md:col-start-1 md:row-start-1 md:col-span-4 md:row-span-2 min-h-[150px]"
+            />
+            
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-              className=""
-            ></CompetitionPicker>
+              link="/competitions/sports/basketball-putri"
+              title="Basketball Putri"
+              alt="rectorcup"
+              className="w-full md:col-start-5 md:row-start-1 md:col-span-2 md:row-span-2 min-h-[150px]"
+            />
+            
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-              className="md:row-span-2"
-            ></CompetitionPicker>
+              link="/competitions/sports/futsal"
+              title="Futsal"
+              alt="rectorcup"
+              className="w-full md:col-start-1 md:row-start-3 md:col-span-2 md:row-span-3 min-h-[150px]"
+            />
+            
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-              className="md:col-span-2"
-            ></CompetitionPicker>
+              link="/competitions/sports/billiard"
+              title="Billiard"
+              alt="rectorcup"
+              className="w-full md:col-start-3 md:row-start-3 md:col-span-2 md:row-span-2 min-h-[150px]"
+            />
+            
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-            ></CompetitionPicker>
+              link="/competitions/sports/badminton"
+              title="Badminton"
+              alt="rectorcup"
+              className="w-full md:col-start-5 md:row-start-3 md:col-span-2 md:row-span-5 min-h-[150px]"
+            />
+            
             <CompetitionPicker
               src="/placeholder/basketball-putra.jpeg"
-              link="/competitions/sports/basketball-putra"
-              title="Basketball Putra"
-              alt="card"
-              className=""
-            ></CompetitionPicker>
+              link="/competitions/sports/ping-pong"
+              title="Ping Pong"
+              alt="rectorcup"
+              className="w-full md:col-start-3 md:row-start-5 md:col-span-2 md:row-span-3 min-h-[150px]"
+            />
+            
+            <CompetitionPicker
+              src="/placeholder/basketball-putra.jpeg"
+              link="/competitions/sports/taekwondo"
+              title="Taekwondo"
+              alt="rectorcup"
+              className="w-full md:col-start-1 md:row-start-6 md:col-span-2 md:row-span-2 min-h-[150px]"
+            />
           </div>
         </div>
         <BottomAsset/>
