1964e973e1a1a1b98d1f1eb7fc0841d9989d3672
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-20 17:48:24 +0700

    uppercase signin

--- src/components/auth/GoogleLogin.tsx
+++ src/components/auth/GoogleLogin.tsx
@@ -5,7 +5,7 @@ export default function GoogleLogin() {
     <button
       type="button"
       aria-label="Sign in"
-      className="w-full flex justify-end rounded-lg bg-black/30 backdrop-blur-2xl border-white border-3 px-4 py-2 whitespace-nowrap"
+      className="w-full flex justify-end rounded-lg bg-black/30 backdrop-blur-2xl border-white border-3 px-4 py-2 whitespace-nowrap uppercase"
       onClick={() => signIn("google", { callbackUrl: "/" })}
     >
       Sign in
