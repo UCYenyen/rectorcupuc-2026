407a2bc018f7adf50a199ca48a91fa3178a784f4
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-20 17:44:35 +0700

    fix_navbar

--- src/components/auth/GoogleLogin.tsx
+++ src/components/auth/GoogleLogin.tsx
@@ -5,7 +5,7 @@ export default function GoogleLogin() {
     <button
       type="button"
       aria-label="Sign in"
-      className="w-full flex justify-end rounded-lg bg-zinc-300 items-center whitespace-nowrap px-4 py-2"
+      className="w-full flex justify-end rounded-lg bg-black/30 backdrop-blur-2xl border-white border-3 px-4 py-2 whitespace-nowrap"
       onClick={() => signIn("google", { callbackUrl: "/" })}
     >
       Sign in
