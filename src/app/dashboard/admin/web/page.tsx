4d17dbde293f71605919c9e717380ca175579651
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-21 11:19:11 +0700

    use ciputra email

--- src/app/dashboard/admin/web/page.tsx
+++ src/app/dashboard/admin/web/page.tsx
@@ -14,7 +14,7 @@ export default function page() {
         className="w-full h-full object-center object-cover opacity-25 z-1 absolute"
       ></Image>
       <div className="relative z-10 border-[#AAF3D5] flex flex-col justify-center items-center gap-4 border-4 bg-white/10 backdrop-blur-sm p-12 rounded-2xl shadow-2xl text-center max-w-md w-full">
-        <h1 className='text-3xl font-bold'>Admin Dashboard</h1>
+        <h1 className='text-3xl font-bold text-white'>Admin Dashboard</h1>
         <Link href={"/dashboard/admin/lo/registration"} className='bg-black/30 px-4 py-2 text-white rounded-lg text-center border-white border-4 w-fit'>Manage Registrations</Link>
         <Link href={"/dashboard/admin/web/competitions"} className='bg-black/30 px-4 py-2 text-white rounded-lg text-center border-white border-4 w-fit'>Manage Competitions</Link>
       </div>
