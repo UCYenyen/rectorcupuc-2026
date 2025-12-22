0bfba6372c8f6bdf46bdaaa77e7f7f7ea9240bc2
valeyy <marsha.valerie.wibowo@gmail.com>
2025-12-22 21:08:58 +0700

    update competition arts

---
+++ src/app/competitions/arts/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import CompetitionDetailsDataReciever from "@/components/competition/CompetitionDetailsDataReciever";
+import { getCompetitionBySlug } from "@/lib/competition";
+import { redirect } from "next/dist/client/components/navigation";
+import { auth } from "@/lib/auth";
+import { checkUserRegistrationStatus } from "@/lib/competition";
+
+interface ProblemPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function BasketballPutraPage({ params }: ProblemPageProps) {
+  const { slug } = await params;
+  const competitionData = await getCompetitionBySlug(slug);
+  const session = await auth();
+
+  if (!competitionData) {
+    return redirect('/not-found');
+  }
+
+  if(!session){
+    return redirect('/');
+  }
+
+  return;
+}
--- src/app/competitions/arts/page.tsx
+++ src/app/competitions/arts/page.tsx
@@ -1,86 +1,70 @@
 import React from "react";
 import Image from "next/image";
 import CompetitionPicker from "@/components/competition/CompetitionPicker";
-// import { getAllCompetitions } from '@/lib/competition'
-// import CompetitionContainer from '@/components/competition/CompetitionContainer';
+import StripeBackground from "@/components/StripeBackground";
+import RectorInlineTitle from "@/components/competition/RectorInlineTitle";
+import CompetitionTitleHeader from "@/components/competition/CompetitionTitleHeader";
 
 export default async function page() {
-  // const competitionData = await getAllCompetitions();
   return (
     <>
       <div className='w-screen h-[10vh] md:h-[7vh]'></div>
       <div className="relative min-h-screen bg-[url('/home/background.svg')] w-screen overflow-hidden flex flex-col justify-center items-center">
         <div className="absolute w-full h-full bg-gradient-to-b from-[#390D62] to-[#6226A4] z-[1] overflow-hidden"></div>
-        <Image src={"/home/background.svg"} width={1000} height={1000} alt="rectorcupuc background" className="w-full h-full object-center object-cover z-1 absolute"></Image>
-        <div className="relative z-2 mt-[10%] flex flex-col gap-4 w-[90%] justify-center items-center border-4 border-[#AAF3D5] p-4 rounded-lg shadow-lg backdrop-blur-2xl bg-gradient-to-b from-[#390D62]/40 to-[#6226A4]/40">
-          <Image
-            src={"/logos/rector-title.svg"}
-            width={600}
-            height={600}
-            alt="Rectorcupuc Title"
-            className="absolute w-1/4 h-auto -top-[6rem]"
-          ></Image>
-          <div className="w-full border-4 border-[#AAF3D5] rounded-2xl py-2 bg-gradient-to-r from-[#6427A8] to-[#EB79F0] flex justify-center items-center">
-            <h1 className="text-2xl">COMPETITIONS</h1>
-          </div>
-          <div className="hidden md:grid xl:hidden grid-cols-2 gap-4">
+        <StripeBackground />
+        <div className="relative z-2 my-[7.5rem] md:my-[10%] flex flex-col gap-4 w-[90%] justify-center items-center border-8 border-[#AAF3D5] p-4 md:p-12 rounded-lg shadow-lg backdrop-blur-2xl bg-gradient-to-b from-[#390D62]/40 to-[#6226A4]/40">
+        <RectorInlineTitle />
+        <CompetitionTitleHeader title="SPORTS" shouldFitContent={false} />
+          <div className="w-full grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-3 md:h-[70vh]">
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/dance.webp"
+              link="/competitions/arts"
+              title="Dance"
+              alt="rectorcupuc dance"
+              className="w-full h-full md:row-span-2"
+            />
+
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/solo-singing.webp"
+              link="/competitions/arts"
+              title="Solo Singing"
+              alt="rectorcupuc solo-singing"
+              className="w-full h-full"
+            />
+
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/duet-singing.webp"
+              link="/competitions/arts"
+              title="Duet Singing"
+              alt="rectorcupuc duet-singing"
+              className="w-full h-full md:row-span-2"
+            />
+
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/photography.webp"
+              link="/competitions/arts"
+              title="Photography"
+              alt="rectorcupuc photography"
+              className="w-full h-full"
+            />
+
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/poster.webp"
+              link="/competitions/arts"
+              title="Poster"
+              alt="rectorcupuc poster"
+              className="w-full h-full"
+            />
+
             <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-          </div>
-          <div className="grid grid-cols-1 sm:hidden sm:grid-cols-2 xl:grid-cols-3 gap-4">
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-          </div>
-          <div className="grid grid-cols-1 sm:hidden sm:grid-cols-2 xl:grid-cols-3 gap-4">
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
-            <CompetitionPicker
-              src="/placeholder/football.svg"
-              alt="card"
-            ></CompetitionPicker>
+              src="/competitions/hero/poetry.webp"
+              link="/competitions/arts"
+              title="Poetry"
+              alt="rectorcupuc poetry"
+              className="w-full h-full"
+            />
           </div>
         </div>
-        {/* <div className="w-full max-w-5xl">
-        <CompetitionContainer items={competitionData} />
-      </div> */}
       </div>
     </>
   );
