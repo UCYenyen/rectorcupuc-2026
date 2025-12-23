030ada1df04840013368dfe73042755aeeb4904e
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-23 22:56:41 +0700

    changed_under_development

--- src/app/underdevelopment/page.tsx
+++ src/app/underdevelopment/page.tsx
@@ -10,8 +10,8 @@ export default function UnderDevelopmentPage() {
       <div className="relative min-h-screen bg-[url('/home/background.svg')] w-screen overflow-hidden flex flex-col justify-center items-center">
         <div className="absolute w-full h-full bg-gradient-to-b from-[#390D62] to-[#6226A4] z-[1] overflow-hidden"></div>
         <StripeBackground />
+         <RectorInlineTitle />
         <div className="relative z-2 my-[7.5rem] md:my-[10%] flex flex-col gap-4 w-[90%] justify-center items-center border-8 border-[#AAF3D5] p-8 md:p-12 rounded-lg shadow-lg backdrop-blur-2xl bg-gradient-to-b from-[#390D62]/40 to-[#6226A4]/40">
-          <RectorInlineTitle />
           <CompetitionTitleHeader title="UNDER DEVELOPMENT" shouldFitContent={true} />
 
           <div className="flex flex-col justify-center items-center gap-4">
