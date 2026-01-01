bac095315d2ab37e24e4f42063f512f200806ccf
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-01 16:39:47 +0700

    next-config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+    // Allow local uploads
+    unoptimized: false,
+  },
+  // Don't include uploads in static build
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
