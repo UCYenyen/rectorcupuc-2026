55c37724a486992fe23d531bd965d1614d742bc5
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-02 19:23:06 +0700

    add_baseline_depedencies

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
-    // Allow local uploads
-    unoptimized: false,
-  },
-  // Don't include uploads in static build
-  output: 'standalone',
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb",
-    },
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "lh3.googleusercontent.com",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "res.cloudinary.com",
+  //     },
+  //   ],
+  //   // Allow local uploads
+  //   unoptimized: false,
+  // },
+  // // Don't include uploads in static build
+  // output: 'standalone',
+  // experimental: {
+  //   serverActions: {
+  //     bodySizeLimit: "10mb",
+  //   },
+  // },
 };
 
 export default nextConfig;
