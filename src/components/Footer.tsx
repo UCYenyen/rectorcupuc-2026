c483e449dabdc85d9f82e133016222a91f1ffb94
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-05 18:25:31 +0700

    add_terms_privacy_policy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BottomAsset from './competition/BottomAsset'
+import Link from 'next/link'
 
 export default function Footer() {
     return (
@@ -10,6 +11,7 @@ export default function Footer() {
             </div>
             <div className='w-full bg-gradient-to-b from-[#390D62] to-[#6226A4] flex flex-col justify-center items-center py-4'>
                 <p className='text-white text-sm sm:text-base'>&copy; 2026 Rector Cup. All rights reserved.</p>
+                <Link href="/privacy-policy" className='text-white text-sm sm:text-base underline mt-2'>Privacy Policy</Link>
             </div>
         </footer>
     )
