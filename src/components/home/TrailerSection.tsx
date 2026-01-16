c50ddd8be4992b5db1ff1f4dbedd8a30c9baf2ff
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-16 21:22:57 +0700

    add video

--- src/components/home/TrailerSection.tsx
+++ src/components/home/TrailerSection.tsx
@@ -8,7 +8,8 @@ export default function TrailerSection() {
       <div className="bg-trailer absolute w-full h-full bg-gradient-to-b from-[#390D62] to-[#6226A4] z-[1] opacity-80"></div>
       <div className='relative z-10 flex flex-col justify-center items-center gap-12'>
         <h1 className='text-5xl font-bold text-white'>TRAILER</h1>
-        <div className="video-frame bg-[url('/home/video-frame.webp')] bg-cover bg-no-repeat bg-center w-[20rem] sm:w-[50rem] h-[12rem] sm:h-[30rem]">
+        <div className="video-frame bg-[url('/home/video-frame.webp')] bg-cover bg-no-repeat bg-center aspect-video w-[20rem] sm:w-[50rem] h-[12rem] sm:h-[30rem] flex justify-center items-center px-4">
+          <iframe width="full" height="auto" src="https://www.youtube.com/embed/3dcP9JfMbXQ?autoplay=1&mute=1" title="RECTOR CUP 2026: OFFICIAL TRAILER" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className='aspect-video w-full h-auto'></iframe>
         </div>
       </div>
       <StripeBackground />
