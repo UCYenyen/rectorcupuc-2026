5ccc96190a9397bed0b26873d87a77ec8cf64305
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-28 14:30:32 +0700

    Add logo uc

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BottomAsset from './competition/BottomAsset'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Footer() {
     return (
@@ -10,6 +11,10 @@ export default function Footer() {
                 <BottomAsset />
             </div>
             <div className='w-full bg-gradient-to-b from-[#390D62] to-[#6226A4] flex flex-col justify-center items-center py-4'>
+                <div className='flex gap-4 items-center justify-center'>
+                    <Image src={"/logos/logo-uc.png"} className='h-auto w-16' alt="Rector Cup Logo UC" width={100} height={50} />
+                    <Image src={"/logos/logo-sc.png"} className='h-auto w-16' alt="Rector Cup Logo SC" width={100} height={50} />
+                </div>
                 <p className='text-white text-sm sm:text-base'>&copy; 2026 Rector Cup. All rights reserved.</p>
                 <Link href="/privacy-policy" className='text-white text-sm sm:text-base underline mt-2'>Privacy Policy</Link>
             </div>
