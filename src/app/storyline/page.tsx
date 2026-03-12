e888e2f0dcb72ed8892763da83241d5fde905223
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-12 10:04:34 +0700

    mounting volume image

--- src/app/storyline/page.tsx
+++ src/app/storyline/page.tsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+   <div className='min-h-screen w-screen overflow-hidden flex flex-col justify-center items-center'>
+      <h1 className='text-4xl font-bold'>Storyline</h1>
+      <table className='relative z-1 border-collapse border-1 border-white'>
+        <div className='border-collapse border-1 p-2'>
+        <tr className='border-1 border-white'>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+        </tr>
+        <tr className='border-1 border-white'>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+        </tr>
+        <tr className='border-1 border-white'>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+          <td className='border-1 border-white'>a</td>
+        </tr>
+        </div>
+      </table>
+   </div>
   )
 }
